4f273953aff612c4c588195f4ca69fa602f122a6
bmt2211 <bmt221100@gmail.com>
2026-05-02 15:20:04 +0700

    [260502] Unit Test part 1

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,15 @@ mod bandwith_calculator;
 fn main() {
     bandwith_calculator::run();
 }
+
+// --- UNIT TESTS ---
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    #[test]
+    fn test_calculate() {
+        let result = 4*25;
+        assert_eq!(result,100);
+    }
+}
