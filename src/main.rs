6f387587f668dd08c928fe47282dc524804cba35
bmt2211 <bmt221100@gmail.com>
2026-05-03 21:59:25 +0700

    [260503] Separte feature option for each requirement

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ fn main() {
     //bandwith_calculator::run();
     
     // Requirement 1
-    let sensor1 = sensor_debugger::SensorData::new(100, 1, 25.5);
-    println!("{}",sensor1);
+    #[cfg(feature = "req1")]
+    {
+        let sensor1 = sensor_debugger::SensorData::new(100, 1, 25.5);
+        println!("Actual Display output{}",sensor1);
+        println!("Actual Debug output: {:?}", sensor1);
+    }
+
+    // Requirement 2
+    #[cfg(feature = "req2")]
+    {
+        println!("T.B.D");
+    }
 }
