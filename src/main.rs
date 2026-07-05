ade6f91a75afcedd83ae6f2789166b75528efbc5
bmt2211 <bmt221100@gmail.com>
2026-07-05 19:12:12 +0700

    Restructure and fix compiler errors

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,14 @@
 mod modules;
 mod ai_stack;
-mod handson;
+mod runner;
 
-use crate::modules::sensor_types::SensorError;
-use colored::*;
-use std::time::{Duration, Instant};
-use std::thread;
+use crate::modules::sensor_types::SensorData;
+use crate::modules::sensor_cacher::SensorCache;
+use crate::ai_stack::agent;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("{}","--- INTIALIZE AI AGENT (RULE-BASED) ---".yellow().bold());
-    
-    #[cfg(feature="ai")]
-    {
-        let _ = ai_stack::agent::run_agent().await;
-    }
-    
-    // Requirement 1
-    #[cfg(feature = "req1")]
-    {
-        let sensor1 = modules::sensor_debugger::SensorData::new(100, 1, 25);
-        println!("Actual Display output{}",sensor1);
-        println!("Actual Debug output: {:?}", sensor1);
-    }
-    
-    #[cfg(feature = "req2")]
-    {
-        let rx_frame = [0xAB,0x01,0x20,0xFF,0x54,0x12,0x00,0xAB,0x99,0x10,0x1F];
-        let rx_data = crate::modules::sensor_types::SensorData::parse
-                                                        (&rx_frame);
-        
-        match rx_data {
-            Ok(data) => {
-                let interpreted_data = modules::sensor_debugger::SensorData::new(
-                                                    data.timestamp,
-                                                    data.id,
-                                                    data.value);
-                println!("Interpretted data {}",interpreted_data);
-            }
-            Err(e) => {
-                println!("Dropped packet due to error: {:?}", e);
-            }
-        }
-    }
-
-    // External Requirement - Cache
-    #[cfg(feature = "req3")]
-    {
-        let adc_reader = || {
-            thread::sleep(Duration::from_millis(50));
-            println!("Read from ADC");
-            95
-        };
-
-        let mut engine_temp = modules::sensor_cacher::SensorCache::new(adc_reader,Duration::from_secs(1));
-
-        println!("--- 1: System initialize, No Cache ---");
-        let t1 = engine_temp.get_value();
-        assert_eq!(t1, 95);
-
-        println!("\n--- 2: Immediately invoked (after 100ms) -> get from Cache ---");
-        thread::sleep(Duration::from_millis(100));
-        let t2 = engine_temp.get_value();
-        assert_eq!(t2, 95);
-
-        println!("\n---3: Longer time (after 1.2s) -> Cache timeout and read hw again ---");
-        thread::sleep(Duration::from_millis(1200));
-        let t3 = engine_temp.get_value();
-        assert_eq!(t3, 95);
-        
-        println!("\nCongrates.");
-    }
+    runner::run().await;
     Ok(())
 }
 
---
+++ src/runner.rs
@@ -0,0 +1,89 @@
+use colored::*;
+use std::time::{Duration, Instant};
+use std::thread;
+
+
+use crate::SensorData;
+use crate::SensorCache;
+use crate::agent;
+
+pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
+
+    #[cfg(feature = "ai")]
+    run_ai().await;
+
+    #[cfg(feature = "req1")]
+    run_req1();
+
+    #[cfg(feature = "req2")]
+    run_req2();
+
+    #[cfg(feature = "req3")]
+    run_req3();
+
+    Ok(())
+}
+
+    #[cfg(feature = "ai")]
+    pub async fn run_ai()
+    {
+        let _ = agent::run_agent().await;
+    }
+    
+    // Requirement 1
+    #[cfg(feature = "req1")]
+    pub fn run_req1()
+    {
+        let sensor1 = SensorData::new(100, 1, 25);
+        println!("Actual Display output{}",sensor1);
+        println!("Actual Debug output: {:?}", sensor1);
+    }
+    
+    #[cfg(feature = "req2")]
+    pub fn run_req2()
+    {
+        let rx_frame = [0xAB,0x01,0x20,0xFF,0x54,0x12,0x00,0xAB,0x99,0x10,0x1F];
+        let rx_data = SensorData::parse(&rx_frame);
+        
+        match rx_data {
+            Ok(data) => {
+                let interpreted_data = SensorData::new(
+                                                    data.timestamp,
+                                                    data.id,
+                                                    data.value);
+                println!("Interpretted data {}",interpreted_data);
+            }
+            Err(e) => {
+                println!("Dropped packet due to error: {:?}", e);
+            }
+        }
+    }
+
+    // External Requirement - Cache
+    #[cfg(feature = "req3")]
+    pub fn run_req3()
+    {
+        let adc_reader = || {
+            thread::sleep(Duration::from_millis(50));
+            println!("Read from ADC");
+            95
+        };
+
+        let mut engine_temp = SensorCache::new(adc_reader,Duration::from_secs(1));
+
+        println!("--- 1: System initialize, No Cache ---");
+        let t1 = engine_temp.get_value();
+        assert_eq!(t1, 95);
+
+        println!("\n--- 2: Immediately invoked (after 100ms) -> get from Cache ---");
+        thread::sleep(Duration::from_millis(100));
+        let t2 = engine_temp.get_value();
+        assert_eq!(t2, 95);
+
+        println!("\n---3: Longer time (after 1.2s) -> Cache timeout and read hw again ---");
+        thread::sleep(Duration::from_millis(1200));
+        let t3 = engine_temp.get_value();
+        assert_eq!(t3, 95);
+        
+        println!("\nCongrates.");
+    }
